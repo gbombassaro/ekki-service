19e04cb8131a0d3bf97feece934144587a14855c
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2021-04-10 17:15:51 -0300

    Adiciona limite de crédito no schema do user

--- server/models/user.js
+++ server/models/user.js
@@ -25,6 +25,10 @@ const UserSchema = new Schema({
     type: Number,
     required: false
   },
+  creditLimit: {
+    type: Number,
+    required: true
+  },
   transactionHistory: {
     type: Array,
     required: false,
